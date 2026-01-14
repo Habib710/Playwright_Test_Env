4e38567eb59cffe0bc9da51b83b34ea0c787e5dc
Habib710 <mdhabib71024@gmail.com>
2026-01-14 15:59:15 +0600

    ned object added

--- tests/pageObjects/HomePage/HomePageObject.js
+++ tests/pageObjects/HomePage/HomePageObject.js
@@ -2,6 +2,9 @@ export class HomePageObjects {
 
     constructor(page) {
         this.page = page;
+        this.accounticon = this.page.locator("//a[@href='https://demo.evershop.io/']");
+        this.shopNow = this.page.locator("//*[name()='path' and contains(@d,'M32.388 18')]");
+        this.shopNow = this.page.locator("//*[name()='path' and contains(@d,'M32.388 18')]");
 
 
     }
