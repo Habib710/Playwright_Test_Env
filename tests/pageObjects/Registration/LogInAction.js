b225ab000a9dea6881ff2ee181419abb2a55696f
Habib710 <mdhabib71024@gmail.com>
2026-01-14 15:03:07 +0600

    new file added

--- tests/pageObjects/Registration/LogInAction.js
+++ tests/pageObjects/Registration/LogInAction.js
@@ -3,27 +3,27 @@ import { LogInObject } from "./LogInObject";
 
 export class LogInInFormation {
 
-constructor(page) {
-    this.page = page;
-    this.locator = new LogInObject(page);
-  }
+    constructor(page) {
+        this.page = page;
+        this.locator = new LogInObject(page);
+    }
 
 
-async clickOnAccountIcon(){
-    await this.locator.accounticon.click();
-}
-async enter_Email(email){
+    async clickOnAccountIcon() {
+        await this.locator.accounticon.click();
+    }
+    async enter_Email(email) {
 
-await this.locator.logInEmail.fill(email);
-await this.locator.logInEmail.fill(email);
+        await this.locator.logInEmail.fill(email);
+        await this.locator.logInEmail.fill(email);
 
-}
-async enter_Password(password){
-    await this.locator.logInPasswoard.fill(password);
-}
-async ClickOnSingIn_Button(){
-    await this.locator.logInButton.click();
-}
+    }
+    async enter_Password(password) {
+        await this.locator.logInPasswoard.fill(password);
+    }
+    async ClickOnSingIn_Button() {
+        await this.locator.logInButton.click();
+    }
 
 
 }
